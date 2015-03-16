2054bedc21b2f403546bd07359b38b62213f13c0
Devon Bleak <devon.bleak@gmail.com>
2015-03-16 23:47:18 +0000

    add documentation, some cleanup

--- awsrequest.c
+++ awsrequest.c
@@ -1,8 +1,5 @@
-#include <stdlib.h>
 #include <curl/curl.h>
 #include <json/json.h>
-#include <stdio.h>
-#include <string.h>
 #include <apr-1/apr_errno.h>
 #include <apr-1/apr_pools.h>
 #include <apr-1/apr_tables.h>
@@ -10,6 +7,7 @@
 #include <openssl/evp.h>
 #include <openssl/hmac.h>
 
+// aws credentials
 static struct
 {
 	char access_key_id[128];
@@ -17,12 +15,14 @@ static struct
 	char token[1024];
 } creds;
 
+// buffer for capturing curl responses
 static struct memstruct
 {
 	char *data;
 	size_t size;
 } curlmemstruct;
 
+// callback for capturing curl responses in memory
 static size_t curl_writemem_callback(void *contents, size_t size, size_t nmemb, void *userp)
 {
 	size_t realsize = size * nmemb;
@@ -40,7 +40,8 @@ static size_t curl_writemem_callback(void *contents, size_t size, size_t nmemb, 
 	return realsize;
 }
 
-static int init_aws_creds_from_role()
+// use EC2 instance metadata to get temporary AWS credentials
+int init_aws_creds_from_role()
 {
 	CURL *ch;
 	json_object *credobj, *tmpobj;
@@ -103,6 +104,7 @@ static int init_aws_creds_from_role()
 
 	creds.access_key_id[0] = 0;
 	strncpy(creds.access_key_id, json_object_get_string(tmpobj), sizeof(creds.access_key_id));
+	json_object_put(tmpobj);
 
 	jsonret = json_object_object_get_ex(credobj, "SecretAccessKey", &tmpobj);
 	if(!jsonret)
@@ -110,6 +112,7 @@ static int init_aws_creds_from_role()
 
 	creds.secret_access_key[0] = 0;
 	strncpy(creds.secret_access_key, json_object_get_string(tmpobj), sizeof(creds.secret_access_key));
+	json_object_put(tmpobj);
 
 	jsonret = json_object_object_get_ex(credobj, "Token", &tmpobj);
 	if(!jsonret)
@@ -117,15 +120,15 @@ static int init_aws_creds_from_role()
 
 	creds.token[0] = 0;
 	strncpy(creds.token, json_object_get_string(tmpobj), sizeof(creds.token));
+	json_object_put(tmpobj);
 
+	json_object_put(credobj);
 	free(curlmemstruct.data);
-	curlmemstruct.data = malloc(sizeof(char));
-	*(curlmemstruct.data) = 0;
-	curlmemstruct.size = 0;
 
 	return 0;
 }
 
+// create a new copy of s from pool and return it lower-cased
 static char *pstrtolower(apr_pool_t *pool, const char *s)
 {
 	char *ret = apr_pstrdup(pool, s);
@@ -137,13 +140,17 @@ static char *pstrtolower(apr_pool_t *pool, const char *s)
 	return ret;
 }
 
+// callback for getting apr_table_t * keys
 static int _get_apr_table_keys_callback(void *data, const char *key, const char *value)
 {
 	apr_array_header_t *arr = data;
 
 	*(const char **)apr_array_push(arr) = apr_pstrdup(arr->pool, key);
+
+	return 1;
 }
 
+// return all the keys from an apr_table_t * as an apr_array_header_t *
 static apr_array_header_t *get_apr_table_keys(apr_pool_t *pool, apr_table_t *table)
 {
 	apr_array_header_t *ret = apr_array_make(pool, 16, sizeof(const char *));
@@ -153,20 +160,39 @@ static apr_array_header_t *get_apr_table_keys(apr_pool_t *pool, apr_table_t *tab
 	return ret;
 }
 
-static int _sort_apr_array_callback(const void *v1, const void *v2)
+// combo of an apr memory pool and curl_slist
+static struct {
+	apr_pool_t *pool;
+	struct curl_slist *slist;
+} pool_slist;
+
+// convert apr_table into curl slist for setting HTTP headers
+static int _header_table_to_slist_callback(void *data, const char *key, const char *value)
 {
+	pool_slist.slist = curl_slist_append(pool_slist.slist, apr_pstrcat(pool_slist.pool, key, ": ", value, NULL));
+
+	return 1;
+}
+
+// callback for sorting apr_array_header_t * in place - dereference and compare
+static int _sort_apr_array_case_callback(const void *v1, const void *v2)
+{
+	// dereference the strings to compare
 	const char *s1 = *(const char **)v1;
 	const char *s2 = *(const char **)v2;
 
+	// return the case-insensitive comparison
 	return strcasecmp(s1, s2);
 }
 
-static void sort_apr_array(apr_array_header_t *arr)
+// sort an apr_array_header_t * in place
+static void sort_apr_array_case(apr_array_header_t *arr)
 {
-	qsort(arr->elts, arr->nelts, arr->elt_size, _sort_apr_array_callback);
+	qsort(arr->elts, arr->nelts, arr->elt_size, _sort_apr_array_case_callback);
 }
 
-static char *psha256(apr_pool_t *pool, const char *message, size_t message_size)
+// calculate sha256 hash and hex-encode into a pool-allocated buffer
+static char *psha256hex(apr_pool_t *pool, const char *message, size_t message_size)
 {
 	EVP_MD_CTX *mdctx;
 	const EVP_MD *md;
@@ -176,6 +202,7 @@ static char *psha256(apr_pool_t *pool, const char *message, size_t message_size)
 	
 	ret = apr_pcalloc(pool, (256 / 4 + 1 ) * sizeof(char));
 
+	// calculate the hash
 	OpenSSL_add_all_digests();
 	md = EVP_sha256();
 
@@ -185,10 +212,9 @@ static char *psha256(apr_pool_t *pool, const char *message, size_t message_size)
 	EVP_DigestFinal_ex(mdctx, md_value, &md_len);
 	EVP_MD_CTX_destroy(mdctx);
 
+	// hex encode the output
 	for(i = 0, tmp = ret; i < md_len; i++, tmp += 2)
-	{
 		apr_snprintf(tmp, 3, "%02x", md_value[i]);
-	}
 
 	EVP_cleanup();
 
@@ -230,6 +256,15 @@ CURLcode execute_signed_aws_request(
 		return CURLE_UNSUPPORTED_PROTOCOL;
 	}
 
+	// create a memory pool
+	aprv = apr_pool_create(&pool, NULL);
+	if(aprv != APR_SUCCESS)
+	{
+		*return_data = "Could not create pool.";
+		*return_size = strlen(*return_data);
+		return CURLE_OUT_OF_MEMORY;
+	}
+
 	// build the canonical request
 	/*
 	CanonicalRequest =
@@ -240,15 +275,6 @@ CURLcode execute_signed_aws_request(
 	  SignedHeaders + '\n' +
 	  HexEncode(Hash(RequestPayload))
 	*/
-
-	aprv = apr_pool_create(&pool, NULL);
-	if(aprv != APR_SUCCESS)
-	{
-		*return_data = "Could not create pool.";
-		*return_size = strlen(*return_data);
-		return CURLE_OUT_OF_MEMORY;
-	}
-
 	canonical_request = apr_pstrcat(pool,
 			method, "\n",
 			path, "\n",
@@ -256,20 +282,18 @@ CURLcode execute_signed_aws_request(
 			NULL
 			);
 
-	// canonical headers
+	// make a copy of the headers since we're going to add some of our own
 	realheaders = apr_table_clone(pool, headers);
-	if(apr_table_get(realheaders, "Host") == NULL)
-	{
-		apr_table_set(realheaders, "Host", hostname);
-	}
+	apr_table_set(realheaders, "Host", hostname);
 
 	// calculate datetime
 	now = time(NULL);
 	strftime(datetime, sizeof(datetime), "%Y%m%dT%H%M%SZ", gmtime(&now));
 	apr_table_set(realheaders, "x-amz-date", datetime);
 
+	// add canonical headers
 	header_keys = get_apr_table_keys(pool, realheaders);
-	sort_apr_array(header_keys);
+	sort_apr_array_case(header_keys);
 
 	for(i = 0; i < header_keys->nelts; i++)
 	{
@@ -285,19 +309,19 @@ CURLcode execute_signed_aws_request(
 
 	// add signed headers and payload hash
 	signed_headers = pstrtolower(pool, apr_array_pstrcat(pool, header_keys, ';'));
-	canonical_request = apr_pstrcat(pool, canonical_request, "\n", signed_headers, "\n", psha256(pool, payload, payload_size), NULL);
+	canonical_request = apr_pstrcat(pool, canonical_request, "\n", signed_headers, "\n", psha256hex(pool, payload, payload_size), NULL);
 
 	// build string to sign
 	apr_snprintf(today, 9, "%s", datetime);
 	string_to_sign = apr_pstrcat(pool,
-			"AWS-HMAC-SHA256\n",
+			"AWS4-HMAC-SHA256\n",
 			datetime, "\n",
 			today, "/", region, "/", service, "/aws4_request\n",
-			psha256(pool, canonical_request, strlen(canonical_request)),
+			psha256hex(pool, canonical_request, strlen(canonical_request)),
 			NULL
 			);
 
-	// build our key
+	// build our signing key
 	/*
 	 * kSecret = Your AWS Secret Access Key
 	 * kDate = HMAC("AWS4" + kSecret, Date)
@@ -334,13 +358,11 @@ CURLcode execute_signed_aws_request(
 			md_value, &md_len
 		);
 
-	request_signature = apr_palloc(pool, 2 * md_len + 1);
+	request_signature = apr_pcalloc(pool, 2 * md_len + 1);
 	for(i = 0, tmpcp = request_signature; i < md_len; i++, tmpcp += 2)
 		apr_snprintf(tmpcp, 3, "%02x", md_value[i]);
 
-	if(strlen(creds.token))
-		apr_table_set(realheaders, "X-Amz-Security-Token", creds.token);
-
+	// build our authorization header
 	apr_table_set(realheaders, "Authorization", apr_pstrcat(pool,
 				"AWS4-HMAC-SHA256 Credential=",
 				creds.access_key_id, "/", today, "/", region, "/", service, "/aws4_request, ",
@@ -350,62 +372,52 @@ CURLcode execute_signed_aws_request(
 				)
 			);
 
-	fprintf(stderr, "Authorization header: [%s]\n", apr_table_get(realheaders, "Authorization"));
+	// if we're using temp creds add the token
+	if(strlen(creds.token))
+		apr_table_set(realheaders, "X-Amz-Security-Token", creds.token);
 
-	status = CURLE_OK;
+	// build the actual request
+	ch = curl_easy_init();
+	curl_easy_setopt(ch, CURLOPT_URL, apr_pstrcat(pool, "https://", hostname, path, NULL));
+
+	// for now treat everything like a POST
+	curl_easy_setopt(ch, CURLOPT_POST, 1L);
+	curl_easy_setopt(ch, CURLOPT_CUSTOMREQUEST, method);
+	if(payload_size != 0)
+	{
+		curl_easy_setopt(ch, CURLOPT_POSTFIELDSIZE, payload_size);
+		curl_easy_setopt(ch, CURLOPT_POSTFIELDS, payload);
+	}
+
+	// set request headers
+	pool_slist.pool = pool;
+	pool_slist.slist = NULL;
+	apr_table_do(_header_table_to_slist_callback, NULL, realheaders, NULL);
+	curl_easy_setopt(ch, CURLOPT_HTTPHEADER, pool_slist.slist);
+
+	// capture the output in curlmemstruct
+	curlmemstruct.data = malloc(sizeof(char));
+	curlmemstruct.size = 0;
+
+	curl_easy_setopt(ch, CURLOPT_WRITEFUNCTION, curl_writemem_callback);
+	curl_easy_setopt(ch, CURLOPT_WRITEDATA, &curlmemstruct);
+
+	// execute the request
+	status = curl_easy_perform(ch);
+
+	// copy results to a fresh non-static buffer
+	*return_data = calloc(curlmemstruct.size, 1);
+	memcpy(*return_data, curlmemstruct.data, curlmemstruct.size);
+	*return_size = curlmemstruct.size;
+
+	// clean up
+	curl_slist_free_all(pool_slist.slist);
+	curl_easy_cleanup(ch);
+	free(curlmemstruct.data);
+	curlmemstruct.data = malloc(sizeof(char));
+	curlmemstruct.size = 0;
+	apr_pool_destroy(pool);
 
 	return status;
 }
 
-int main()
-{
-	int rv;
-	apr_pool_t *pool;
-	apr_status_t aprv;
-	apr_table_t *headers, *queryparams;
-	const char *payload = "Action=DescribeVPCs&Version=2014-10-01";
-	CURLcode crv;
-	char *result;
-	size_t result_size;
-	char buf[256];
-
-	rv = init_aws_creds_from_role();
-
-	if(rv != 0)
-	{
-		fprintf(stderr, "something went wrong...\n");
-		return 1;
-	}
-
-	apr_initialize();
-
-	aprv = apr_pool_create(&pool, NULL);
-	if(aprv != APR_SUCCESS)
-	{
-		fprintf(stderr, "Could not create pool: %s\n", apr_strerror(aprv, buf, sizeof(buf)));
-		return 1;
-	}
-
-	queryparams = NULL;
-
-	headers = apr_table_make(pool, 16);
-	apr_table_set(headers, "Content-Type", "application/x-www-form-urlencoded");
-
-	crv = execute_signed_aws_request(
-			&result,
-			&result_size,
-			"POST",
-			"ec2",
-			"us-east-1",
-			"ec2.us-east-1.amazonaws.com",
-			"/",
-			NULL,
-			headers,
-			payload,
-			strlen(payload)
-			);
-
-	apr_terminate();
-
-	return 0;
-}
